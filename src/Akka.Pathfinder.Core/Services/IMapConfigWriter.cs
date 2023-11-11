4ebe32778703162e161f8ef627330411593ad4d7
Jan <jan.schloots@bluewin.ch>
2023-11-11 11:06:26 +0100

    fix

--- src/Akka.Pathfinder.Core/Services/IMapConfigWriter.cs
+++ src/Akka.Pathfinder.Core/Services/IMapConfigWriter.cs
@@ -13,5 +13,11 @@ public class MapConfigWriter : MapConfigReader, IMapConfigWriter
     public MapConfigWriter(IMongoCollection<MapConfig> collection) : base(collection)
     { }
 
-    public bool AddOrUpdate(Guid Id, MapConfig config) => Collection.ReplaceOne(x => x.Id == Id, config, new ReplaceOptions() { IsUpsert = true }).IsAcknowledged;
+    public bool AddOrUpdate(Guid Id, MapConfig config)
+    {
+        var updater = new UpdateDefinitionBuilder<MapConfig>()
+        .Set(x => x.PointConfigsIds, config.PointConfigsIds)
+        .Set(x => x.Count, config.Count);       
+        return Collection.UpdateOne(x => x.Id == Id, updater, new UpdateOptions() { IsUpsert = true }).IsAcknowledged;
+    }
 }
