134a40726ccd9169b4846d532e33cc9bb72b4a07
st0o0 <kekw@kekw.kekw>
2024-02-23 12:12:58 +0100

    add grpc generator service

---
+++ src/Akka.Pathfinder/Conversions/Conversions.Generator.cs
@@ -0,0 +1,7 @@
+﻿namespace Akka.Pathfinder;
+
+public partial class Conversions
+{
+    public static Layout.MapSettings To(this Grpc.MapSettings settings)
+        => new(settings.PointCost, settings.DefaultDirectionCost, new Layout.MapSize(settings.MapSize.Width, settings.MapSize.Height, settings.MapSize.Depth), [], new Random().Next(int.MinValue, int.MaxValue));
+}
